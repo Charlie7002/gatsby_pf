8c7207ba255c06b856cab3701f6f4b9a5f2e9681
cha <chaced@orange.fr>
2021-08-25 14:05:05 +0200

    change particles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import React from "react"
+import ParticlesComp from "../components/ParticlesComp"
+import "../assets/css/main.css"
 
 import Layout from "../components/Layout"
 
 export default function Home() {
   return (
     <Layout>
+      <ParticlesComp />
       <h1>Salut les batards!</h1>
     </Layout>
   )
