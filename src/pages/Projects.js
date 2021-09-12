d9e0d60cc691eec641703c20319ad2cbb2c25ab0
cha <chaced@orange.fr>
2021-09-12 23:44:50 +0200

    links hero

---
+++ src/pages/Projects.js
@@ -0,0 +1,17 @@
+import React from "react"
+import styled from "styled-components"
+import Layout from "../components/Layout"
+
+const ProjectsStyles = styled.div``
+
+const Projects = () => {
+  return (
+    <Layout>
+      <ProjectsStyles>
+        <p> Salut les projects de ouf ouaich</p>
+      </ProjectsStyles>
+    </Layout>
+  )
+}
+
+export default Projects
